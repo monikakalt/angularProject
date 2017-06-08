1ef5cfb9abd341c761e7cab78a77221073f567d5
Monika <Monika@LAPTOP-29TPTBFC>
2017-06-08 21:56:54 +0300

    Web API

---
+++ APM.Client/Services/services.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module("services",
+                            ["ngResource"])
+                    .constant("appSettings",
+        {
+            serverPath: "http://localhost:58586/"
+        });
+                    
+
+}());
